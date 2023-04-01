9c99095e37f5123116a18d487b3e0cc1f803b42b
weeksjessica <114619757+weeksjessica@users.noreply.github.com>
2023-03-31 20:17:01 -0400

    Readded Globals

---
+++ assets/files/GLOBALS.PY
@@ -0,0 +1,8 @@
+
+# Arrays
+teamMembers = ["Parker Phelps - Product Owner", "Christian Fortin - Scrum Master", "Matthew Burrus - Program Analyst", "Jessica Weeks - QA Developer", "Destan Hutcherson - Program Analyst", "Flora Cherotich - Database Technician", "Adia Chue - Web Developer"]
+
+# This is used to store certain values and make use of them later
+currentEmployeeID = []
+currentUserID = []
+currentUsername = []

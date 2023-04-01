870e50c40fdebd4319440b09d753f7aab57cd76f
weeksjessica <114619757+weeksjessica@users.noreply.github.com>
2023-03-31 20:18:47 -0400

    Trying to merge

--- assets/files/GLOBALS.PY
+++
@@ -1,8 +0,0 @@
-
-# Arrays
-teamMembers = ["Parker Phelps - Product Owner", "Christian Fortin - Scrum Master", "Matthew Burrus - Program Analyst", "Jessica Weeks - QA Developer", "Destan Hutcherson - Program Analyst", "Flora Cherotich - Database Technician", "Adia Chue - Web Developer"]
-
-# This is used to store certain values and make use of them later
-currentEmployeeID = []
-currentUserID = []
-currentUsername = []

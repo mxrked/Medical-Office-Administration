3d96809bab3547b152e6133ac29779561839883b
Parker <pp101socials@gmail.com>
2023-04-04 14:36:29 -0400

    Created file

--- frontend/ui/assets/files/NAVIGATION_FUNCS.py
+++ frontend/ui/assets/files/NAVIGATION_FUNCS.py
@@ -1,31 +1,58 @@
+import SchedulingAppointmentsWindow, StartWindow, ApptRequest, NewPatient, Referrals, LabOrders, CheckIn, CheckOut
+
 from frontend.ui.assets.files.GLOBALS import currentUsername, currentUserID, currentEmployeeID
+from backend.data_manager import DataManger
+
+def hideAllWindowsExceptForAppointments():
+        from frontend import ApptRequest, NewPatient, Referrals, LabOrders, CheckIn, CheckOut
+
+        StartWindow.UIWindow.hide()
+        NewPatient.UIWindow.hide()
+        Referrals.UIWindow.hide()
+        CheckIn.UIWindow.hide()
+        CheckOut.UIWindow.hide()
+        ApptRequest.UIWindow.hide()
+        LabOrders.UIWindow.hide()
+
+def changeTitleText(num, window):
+                # Title
+                if num == 1:
+                    window.UIWindow.setWindowTitle("Forsyth Family Practice Center - Scheduling Appointments -|- User: " + currentUsername[0])
+                if num == 2:
+                    window.UIWindow.setWindowTitle("Forsyth Family Practice Center - Check In -|- User: " + currentUsername[0])
+                if num == 3:
+                    window.UIWindow.setWindowTitle("Forsyth Family Practice Center - Check Out -|- User: " + currentUsername[0])
+                if num == 4:
+                    window.UIWindow.setWindowTitle("Forsyth Family Practice Center - Make Referral -|- User: " + currentUsername[0])
+                if num == 5:
+                    window.UIWindow.setWindowTitle("Forsyth Family Practice Center - Lab Orders -|- User: " + currentUsername[0])
+                if num == 6:
+                    window.UIWindow.setWindowTitle("Forsyth Family Practice Center - Approve Appointment via Portal -|- User: " + currentUsername[0])
+                if num == 7:
+                    window.UIWindow.setWindowTitle("Forsyth Family Practice Center - New Patient -|- User: " + currentUsername[0])
+
+def changeCurrentUserLabelText(window):
+                window.UIWindow.currentUserLabel.setText("Current User: " + currentUsername[0])
+
 
 def enterStartWindow(self):
                 from frontend import StartWindow
 
                 print("Routing to start screen")
-                StartWindow.UIWindow.show()
                 self.hide()
+                StartWindow.UIWindow.show()
+
 def enterSchedulingAppointmentsWindow(self):
                 from frontend import SchedulingAppointmentsWindow
 
                 print("Routing to scheduling appointments screen")
+                hideAllWindowsExceptForAppointments()
                 SchedulingAppointmentsWindow.UIWindow.show()
-                self.hide()
-def enterNewPatientWindow(self):
-                from frontend import NewPatient
-                print("Routing to new patient screen")
+                hideAllWindowsExceptForAppointments()
 
-                if len(currentUsername) == 0:
-                        print("There is nobody logged in..")
-                if len(currentUsername) == 1:
-                        print("Current User: " + currentUsername[0])
+                changeCurrentUserLabelText(SchedulingAppointmentsWindow)
+                changeTitleText(1, SchedulingAppointmentsWindow)
 
-                        NewPatient.UIWindow.show()
-                        self.hide()
-
-
-                # Display new window code here
 def enterCheckInWindow(self):
                 from frontend import CheckIn
                 print("Routing to check in screen")
@@ -35,10 +62,12 @@ def enterCheckInWindow(self):
                 if len(currentUsername) == 1:
                         print("Current User: " + currentUsername[0])
 
-                        CheckIn.UIWindow.show()
                         self.hide()
+                        CheckIn.UIWindow.show()
 
-                # Display new window code here
+                        changeCurrentUserLabelText(CheckIn)
+                        changeTitleText(2, CheckIn)
+
 def enterCheckOutWindow(self):
                 from frontend import CheckOut
                 print("Routing to check out screen")
@@ -48,10 +77,12 @@ def enterCheckOutWindow(self):
                 if len(currentUsername) == 1:
                         print("Current User: " + currentUsername[0])
 
-                        CheckOut.UIWindow.show()
                         self.hide()
+                        CheckOut.UIWindow.show()
 
-                # Display new window code here
+                        changeCurrentUserLabelText(CheckOut)
+                        changeTitleText(3, CheckOut)
+
 def enterMakeReferralWindow(self):
                 from frontend import Referrals
                 print("Routing to make referral screen")
@@ -61,11 +92,12 @@ def enterMakeReferralWindow(self):
                 if len(currentUsername) == 1:
                         print("Current User: " + currentUsername[0])
 
-                        Referrals.UIWindow.show()
                         self.hide()
+                        Referrals.UIWindow.show()
 
+                        changeCurrentUserLabelText(Referrals)
+                        changeTitleText(4, Referrals)
 
-                # Display new window code here
 def enterLabOrdersWindow(self):
                 from frontend import LabOrders
                 print("Routing to lab orders screen")
@@ -75,10 +107,12 @@ def enterLabOrdersWindow(self):
                 if len(currentUsername) == 1:
                         print("Current User: " + currentUsername[0])
 
-                        LabOrders.UIWindow.show()
                         self.hide()
+                        LabOrders.UIWindow.show()
 
-                # Display new window code here
+                        changeCurrentUserLabelText(LabOrders)
+                        changeTitleText(5, LabOrders)
+
 def enterAppointmentApproveViaPortalWindow(self):
                 from frontend import ApptRequest
                 print("Routing to appointment approve via portal screen")
@@ -88,19 +122,45 @@ def enterAppointmentApproveViaPortalWindow(self):
                 if len(currentUsername) == 1:
                         print("Current User: " + currentUsername[0])
 
-                        ApptRequest.UIWindow.show()
                         self.hide()
+                        ApptRequest.UIWindow.show()
+
+                        changeCurrentUserLabelText(ApptRequest)
+                        changeTitleText(6, ApptRequest)
+
+def enterNewPatientWindow():
+                from frontend import NewPatient, SchedulingAppointmentsWindow
+                print("Routing to new patient screen")
+
+                if len(currentUsername) == 0:
+                        print("There is nobody logged in..")
+                if len(currentUsername) == 1:
+                        print("Current User: " + currentUsername[0])
+
+                        SchedulingAppointmentsWindow.UIWindow.hide()
+                        NewPatient.UIWindow.show()
+                        SchedulingAppointmentsWindow.UIWindow.hide()
+
+                        changeCurrentUserLabelText(NewPatient)
+                        changeTitleText(7, NewPatient)
 
 def logoutUser(self):
 
                 # Clearing array values
-                # currentUsername.clear()
-
-                currentUsername[0] = "Test"
+                currentUsername.clear()
+                currentUsername.append("Test")
                 currentUserID.clear()
                 currentEmployeeID.clear()
 
+
                 print("LOGGED OUT")
 
+                print(currentUsername[0])
+
+                self.session.close()
+                self.session.bind.dispose()
+
                 # Routing user back to the start window
+                self.currentUserLabel.setText("No User Logged In")
+
                 enterStartWindow(self)

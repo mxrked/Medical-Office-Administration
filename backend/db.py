7904147f359ebfdafb9e19a45e2461040c0be38f
weeksjessica <114619757+weeksjessica@users.noreply.github.com>
2023-04-06 13:58:38 -0400

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -2,6 +2,8 @@
 db.py - A set of functions for working with the clinics database.
 Author: Jessica Weeks, Christian Fortin
 """
+import data_manager
+import models
 
 if __name__ == "__main__":
     # Add test code

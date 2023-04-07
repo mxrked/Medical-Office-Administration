7c587bc72e9b392b5c91a6aa3f6e8b82699404ff
Parker <pp101socials@gmail.com>
2023-04-07 15:28:21 -0400

    Altered listeners

--- frontend/ui/assets/files/SCHEDULING_LISTENERS.py
+++ frontend/ui/assets/files/SCHEDULING_LISTENERS.py
@@ -5,14 +5,13 @@ This will hold all of the different listeners for the scheduling/rescheduling/ca
 '''
 
 from frontend.ui.assets.files.GLOBALS import *
-
 from PyQt5.QtCore import QDate
 
 
 # Scheduling/Make
 def clearInputs_SA(self):
 
-                defaultDate = QDate(2000, 1, 1)
+                defaultDate = QDate().currentDate()
 
                 self.SA_PatientFNLineEdit.setText("")
                 self.SA_PatientLNLineEdit.setText("")
@@ -26,7 +25,7 @@ def clearInputs_SA(self):
                 self.SA_CurrentAvailableTimesListWidget.clear()
 
                 self.SA_PatientFNLineEdit.setStyleSheet("""
-                
+
                     QLineEdit {
                         border-image: none;
                         border: none;
@@ -37,10 +36,10 @@ def clearInputs_SA(self):
                         padding-left: 10px;
                         padding-right: 10px;
                     }
-                
+
         """)
                 self.SA_PatientLNLineEdit.setStyleSheet("""
-                
+
                     QLineEdit {
                         border-image: none;
                         border: none;
@@ -51,10 +50,10 @@ def clearInputs_SA(self):
                         padding-left: 10px;
                         padding-right: 10px;
                     }
-                
+
         """)
                 self.SA_PatientDOBDateEdit.setStyleSheet("""
-                
+
                     QDateEdit {
                         border-image: none;
                         border: none;
@@ -65,10 +64,10 @@ def clearInputs_SA(self):
                         padding-left: 10px;
                         padding-right: 10px;
                     }
-                
+
         """)
                 self.SA_OfficeLocationsComboBox.setStyleSheet("""
-                
+
                     QComboBox {
                         border-image: none;
                         border: none;
@@ -77,10 +76,10 @@ def clearInputs_SA(self):
                         font-family: "MS Shell Dlg 2";
                         font-size: 11;
                     }
-                
+
         """)
                 self.SA_AppointmentReasonLineEdit.setStyleSheet("""
-                
+
                     QLineEdit {
                         border-image: none;
                         border: none;
@@ -91,10 +90,10 @@ def clearInputs_SA(self):
                         padding-left: 10px;
                         padding-right: 10px;
                     }
-                
+
         """)
                 self.SA_AppointmentTypesComboBox.setStyleSheet("""
-                
+
                     QComboBox {
                         border-image: none;
                         border: none;
@@ -103,10 +102,10 @@ def clearInputs_SA(self):
                         font-family: "MS Shell Dlg 2";
                         font-size: 11;
                     }
-                
+
         """)
                 self.SA_PhysicianNamesComboBox.setStyleSheet("""
-                
+
                     QComboBox {
                         border-image: none;
                         border: none;
@@ -115,10 +114,10 @@ def clearInputs_SA(self):
                         font-family: "MS Shell Dlg 2";
                         font-size: 11;
                     }
-                
+
         """)
                 self.SA_AppointmentDateDateEdit.setStyleSheet("""
-                
+
                     QDateEdit {
                         border-image: none;
                         border: none;
@@ -129,7 +128,7 @@ def clearInputs_SA(self):
                         padding-left: 10px;
                         padding-right: 10px;
                     }
-                
+
         """)
 def getPatientFN_SA(self):
     return self.SA_PatientFNLineEdit.text()
@@ -193,8 +192,7 @@ def disableCustomTime(self):
 "    padding-right: 10px;\n"
 "}\n")
         print("Disabled Custom Time")
-def search_SA(self):
-
+def checkValidValues_SA(self):
     patientFN = getPatientFN_SA(self)
     patientLN = getPatientLN_SA(self)
     patientDOB = getPatientDOB_SA(self)
@@ -204,15 +202,13 @@ def search_SA(self):
     physicianName = getPhysicianName_SA(self)
     appointmentDate = getAppointmentDate_SA(self)
 
-    # Checking if inputs are/are not empty
-    if patientFN != "" and patientFN != None:
-        print() # Check if is valid and in db
+    # # Checking if inputs are empty
     if patientFN == "" or patientFN == None:
         validPatientFN_SA.clear()
         validPatientFN_SA.append(False)
 
         self.SA_PatientFNLineEdit.setStyleSheet("""
-                
+
                     QLineEdit {
                         border-image: none;
                         border: 2px solid red;
@@ -223,17 +219,15 @@ def search_SA(self):
                         padding-left: 10px;
                         padding-right: 10px;
                     }
-                
+
         """)
 
-    if patientLN != "" and patientLN != None:
-        print() # Check if is valid and in db
     if patientLN == "" or patientLN == None:
         validPatientLN_SA.clear()
         validPatientLN_SA.append(False)
 
         self.SA_PatientLNLineEdit.setStyleSheet("""
-                
+
                     QLineEdit {
                         border-image: none;
                         border: 2px solid red;
@@ -244,20 +238,16 @@ def search_SA(self):
                         padding-left: 10px;
                         padding-right: 10px;
                     }
-                
+
         """)
 
-    # Patient DOB
-
     # Office Location
-    if officeLocation != 0:
-        print() # Check if is valid
     if officeLocation == 0:
         validOfficeLocation_SA.clear()
         validOfficeLocation_SA.append(False)
 
         self.SA_OfficeLocationsComboBox.setStyleSheet("""
-                
+
                     QComboBox {
                         border-image: none;
                         border: 2px solid red;
@@ -268,11 +258,9 @@ def search_SA(self):
                         padding-left: 10px;
                         padding-right: 10px;
                     }
-                
+
         """)
 
-    if appointmentReason != "" and appointmentReason != None:
-        print() # Check if is valid and in db
     if appointmentReason == "" or appointmentReason == None:
 
         validAppointmentReason_SA.clear()
@@ -294,14 +282,12 @@ def search_SA(self):
             """)
 
     # Appointment Type
-    if appointmentType != 0:
-        print() # Check if is valid
     if appointmentType == 0:
         validAppointmentType_SA.clear()
         validAppointmentType_SA.append(False)
 
         self.SA_AppointmentTypesComboBox.setStyleSheet("""
-                
+
                     QComboBox {
                         border-image: none;
                         border: 2px solid red;
@@ -312,18 +298,16 @@ def search_SA(self):
                         padding-left: 10px;
                         padding-right: 10px;
                     }
-                
+
         """)
 
     # Physician Name
-    if physicianName != 0:
-        print() # Check if is valid
     if physicianName == 0:
         validPhysicianName_SA.clear()
         validPhysicianName_SA.append(False)
 
         self.SA_PhysicianNamesComboBox.setStyleSheet("""
-                
+
                     QComboBox {
                         border-image: none;
                         border: 2px solid red;
@@ -334,37 +318,32 @@ def search_SA(self):
                         padding-left: 10px;
                         padding-right: 10px;
                     }
-                
+
         """)
+
+def search_SA(self):
+
+    patientFN = getPatientFN_SA(self)
+    patientLN = getPatientLN_SA(self)
+    patientDOB = getPatientDOB_SA(self)
+    officeLocation = getOfficeLocation_SA(self)
+    appointmentReason = getAppointmentReason_SA(self)
+    appointmentType = getAppointmentType_SA(self)
+    physicianName = getPhysicianName_SA(self)
+    appointmentDate = getAppointmentDate_SA(self)
+
+    checkValidValues_SA(self)
 
     # Appointment Date
 def scheduleAppointment(self):
 
-    if validPatientFN_SA[0] == True and validPatientLN_SA[0] == True and validPatientDOB_SA[0] == True and validOfficeLocation_SA[0] == True and validAppointmentReason_SA[0] == True and validAppointmentType_SA[0] == True and validPhysicianName_SA[0] == True and validAppointmentDate_SA[0] == True:
-        print()
+    checkValidValues_SA(self)
 
-    # Checking for invalids
-    if validPatientFN_SA[0] != True:
-        print("Patient FN value is invalid (SA)")
-    if validPatientLN_SA[0] != True:
-        print("Patient LN value is invalid (SA)")
-    if validPatientDOB_SA[0] != True:
-        print("Patient DOB value is invalid (SA)")
-    if validOfficeLocation_SA[0] != True:
-        print("Office Location value is invalid (SA)")
-    if validAppointmentReason_SA[0] != True:
-        print("Appointment Reason value is invalid (SA)")
-    if validAppointmentType_SA[0] != True:
-        print("Appointment Type value is invalid (SA)")
-    if validPhysicianName_SA[0] != True:
-        print("Physician Name value is invalid (SA)")
-    if validAppointmentDate_SA[0] != True:
-        print("Appointment Date value is invalid (SA)")
 
 # Rescheduling
 def clearInputs_RA(self):
 
-                defaultDate = QDate(2000, 1, 1)
+                defaultDate = QDate().currentDate()
 
                 self.RA_OfficeLocationsComboBox.setCurrentIndex(0)
                 self.RA_PhysicianNamesComboBox.setCurrentIndex(0)
@@ -375,7 +354,7 @@ def clearInputs_RA(self):
                 self.RA_CurrentAvailableTimesListWidget.clear()
 
                 self.RA_OfficeLocationsComboBox.setStyleSheet("""
-                
+
                     QComboBox {
                         border-image: none;
                         border: none;
@@ -384,10 +363,10 @@ def clearInputs_RA(self):
                         font-family: "MS Shell Dlg 2";
                         font-size: 11;
                     }
-                
+
         """)
                 self.RA_PhysicianNamesComboBox.setStyleSheet("""
-                
+
                     QComboBox {
                         border-image: none;
                         border: none;
@@ -396,10 +375,10 @@ def clearInputs_RA(self):
                         font-family: "MS Shell Dlg 2";
                         font-size: 11;
                     }
-                
+
         """)
                 self.RA_AppointmentDateDateEdit.setStyleSheet("""
-                
+
                     QDateEdit {
                         border-image: none;
                         border: none;
@@ -410,10 +389,10 @@ def clearInputs_RA(self):
                         padding-left: 10px;
                         padding-right: 10px;
                     }
-                
+
         """)
                 self.RA_RescheduleDateDateEdit.setStyleSheet("""
-                
+
                     QDateEdit {
                         border-image: none;
                         border: none;
@@ -424,7 +403,7 @@ def clearInputs_RA(self):
                         padding-left: 10px;
                         padding-right: 10px;
                     }
-                
+
         """)
 def getOfficeLocation_RA(self):
     return self.RA_OfficeLocationsComboBox.currentIndex()
@@ -434,6 +413,51 @@ def getAppointmentDate_RA(self):
     return self.RA_AppointmentDateDateEdit.date()
 def getRescheduleDate_RA(self):
     return self.RA_RescheduleDateDateEdit.date()
+def checkValidValues_RA(self):
+    officeLocation = getOfficeLocation_RA(self)
+    physicianName = getPhysicianName_RA(self)
+    appointmentDate = getAppointmentDate_RA(self)
+    rescheduleDate = getRescheduleDate_RA(self)
+
+    #Office Location
+    if officeLocation == 0:
+        validOfficeLocation_SA.clear()
+        validOfficeLocation_SA.append(False)
+
+        self.RA_OfficeLocationsComboBox.setStyleSheet("""
+
+                    QComboBox {
+                        border-image: none;
+                        border: 2px solid red;
+                        background-color: #F3ECB0;
+                        color: #344D67;
+                        font-family: "MS Shell Dlg 2";
+                        font-size: 11;
+                        padding-left: 10px;
+                        padding-right: 10px;
+                    }
+
+        """)
+
+    # Physician Name
+    if physicianName == 0:
+        validPhysicianName_SA.clear()
+        validPhysicianName_SA.append(False)
+
+        self.RA_PhysicianNamesComboBox.setStyleSheet("""
+
+                    QComboBox {
+                        border-image: none;
+                        border: 2px solid red;
+                        background-color: #F3ECB0;
+                        color: #344D67;
+                        font-family: "MS Shell Dlg 2";
+                        font-size: 11;
+                        padding-left: 10px;
+                        padding-right: 10px;
+                    }
+
+        """)
 def displayTimesApps_RA(self):
     '''
         This is basically another search
@@ -444,73 +468,17 @@ def displayTimesApps_RA(self):
     appointmentDate = getAppointmentDate_RA(self)
     rescheduleDate = getRescheduleDate_RA(self)
 
-    # Office Location
-    if officeLocation != 0:
-        print() # Check if is valid
-    if officeLocation == 0:
-        validOfficeLocation_SA.clear()
-        validOfficeLocation_SA.append(False)
+    checkValidValues_RA(self)
 
-        self.RA_OfficeLocationsComboBox.setStyleSheet("""
-                
-                    QComboBox {
-                        border-image: none;
-                        border: 2px solid red;
-                        background-color: #F3ECB0;
-                        color: #344D67;
-                        font-family: "MS Shell Dlg 2";
-                        font-size: 11;
-                        padding-left: 10px;
-                        padding-right: 10px;
-                    }
-                
-        """)
-
-    # Physician Name
-    if physicianName != 0:
-        print() # Check if is valid
-    if physicianName == 0:
-        validPhysicianName_SA.clear()
-        validPhysicianName_SA.append(False)
-
-        self.RA_PhysicianNamesComboBox.setStyleSheet("""
-                
-                    QComboBox {
-                        border-image: none;
-                        border: 2px solid red;
-                        background-color: #F3ECB0;
-                        color: #344D67;
-                        font-family: "MS Shell Dlg 2";
-                        font-size: 11;
-                        padding-left: 10px;
-                        padding-right: 10px;
-                    }
-                
-        """)
-
-    # Appointment Date
-
-    # Reschedule Date
 
 def rescheduleAppointment(self):
 
-    if validOfficeLocation_RA[0] == True and validPhysicianName_RA[0] == True and validAppointmentDate_RA[0] == True and validRescheduleDate_RA[0] == True:
-        print()
-
-    # Checking for invalids
-    if validOfficeLocation_RA[0] != True:
-        print("Office Location value is invalid (RA)")
-    if validPhysicianName_RA[0] != True:
-        print("Physician Name value is invalid (RA)")
-    if validAppointmentDate_RA[0] != True:
-        print("Appointment Date value is invalid (RA)")
-    if validRescheduleDate_RA[0] != True:
-        print("Reschedule Appointment Date value is invalid (RA)")
+    checkValidValues_RA(self)
 
 # Cancel
 def clearInputs_CA(self):
 
-                defaultDate = QDate(2000, 1, 1)
+                defaultDate = QDate().currentDate()
 
                 self.CA_OfficeLocationsComboBox.setCurrentIndex(0)
                 self.CA_PhysicianNamesComboBox.setCurrentIndex(0)
@@ -562,67 +530,61 @@ def getPhysicianName_CA(self):
     return self.CA_PhysicianNamesComboBox.currentIndex()
 def getAppointmentDate_CA(self):
     return self.CA_AppointmentDateDateEdit.date()
+def checkValidValues_CA(self):
+    officeLocation = getOfficeLocation_CA(self)
+    physicianName = getPhysicianName_CA(self)
+    appointmentDate = getAppointmentDate_CA(self)
+
+    # Office Location
+    if officeLocation == 0:
+        validOfficeLocation_SA.clear()
+        validOfficeLocation_SA.append(False)
+
+        self.CA_OfficeLocationsComboBox.setStyleSheet("""
+
+                    QComboBox {
+                        border-image: none;
+                        border: 2px solid red;
+                        background-color: #F3ECB0;
+                        color: #344D67;
+                        font-family: "MS Shell Dlg 2";
+                        font-size: 11;
+                        padding-left: 10px;
+                        padding-right: 10px;
+                    }
+
+        """)
+
+    # Physician Name
+    if physicianName == 0:
+        validPhysicianName_SA.clear()
+        validPhysicianName_SA.append(False)
+
+        self.CA_PhysicianNamesComboBox.setStyleSheet("""
+
+                    QComboBox {
+                        border-image: none;
+                        border: 2px solid red;
+                        background-color: #F3ECB0;
+                        color: #344D67;
+                        font-family: "MS Shell Dlg 2";
+                        font-size: 11;
+                        padding-left: 10px;
+                        padding-right: 10px;
+                    }
+
+        """)
 def search_CA(self):
 
     officeLocation = getOfficeLocation_CA(self)
     physicianName = getPhysicianName_CA(self)
     appointmentDate = getAppointmentDate_CA(self)
 
-    # Office Location
-    if officeLocation != 0:
-        print() # Check if is valid
-    if officeLocation == 0:
-        validOfficeLocation_SA.clear()
-        validOfficeLocation_SA.append(False)
+    checkValidValues_CA(self)
 
-        self.CA_OfficeLocationsComboBox.setStyleSheet("""
-                
-                    QComboBox {
-                        border-image: none;
-                        border: 2px solid red;
-                        background-color: #F3ECB0;
-                        color: #344D67;
-                        font-family: "MS Shell Dlg 2";
-                        font-size: 11;
-                        padding-left: 10px;
-                        padding-right: 10px;
-                    }
-                
-        """)
 
-    # Physician Name
-    if physicianName != 0:
-        print() # Check if is valid
-    if physicianName == 0:
-        validPhysicianName_SA.clear()
-        validPhysicianName_SA.append(False)
-
-        self.CA_PhysicianNamesComboBox.setStyleSheet("""
-                
-                    QComboBox {
-                        border-image: none;
-                        border: 2px solid red;
-                        background-color: #F3ECB0;
-                        color: #344D67;
-                        font-family: "MS Shell Dlg 2";
-                        font-size: 11;
-                        padding-left: 10px;
-                        padding-right: 10px;
-                    }
-                
-        """)
-
-    # Appointment Date
 
 def cancelAppointment(self):
 
-    if validOfficeLocation_CA[0] == True and validPhysicianName_CA[0] == True and validAppointmentDate_CA[0] == True:
-        print()
+    checkValidValues_CA(self)
 
-    # Checking for invalids
-    if validOfficeLocation_CA[0] != True:
-        print("Office Location value is invalid (CA)")
-    if validPhysicianName_CA[0] != True:
-        print("Physician Name value is invalid (CA)")
-    if validAppointmentDate_CA[0] != True:
-        print("Appointment Date value is invalid (CA)")

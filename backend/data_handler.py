6af67d3fa1449e49618f3e049c59e56ba53cea1c
weeksjessica <114619757+weeksjessica@users.noreply.github.com>
2023-04-07 14:45:59 -0400

    finalized data handling

--- backend/data_handler.py
+++ backend/data_handler.py
@@ -2,40 +2,68 @@
 data_handler.py - A data handler for placing backend models into front end widgets for P
 Authors: Jessica Weeks,
 """
+from PyQt5.QtWidgets import QListWidget, QListWidgetItem, QComboBox
 
-from PyQt5.QtWidgets import QListWidget, QListWidgetItem
+class __QlistWidgetObject(QListWidgetItem):
+    """
+        Used for ListWidgets to present a str to the GUI
+        but when we call .selectedItems() we can get the data (the obj) back
+    """
+    def __init__(self, obj):
+        super().__init__(str(obj))
+        self.obj = obj
 
-example_objects = []
-
-class test_object():
+def set_objects_to_list(objects: list[object], list_widget: QListWidget):
+    """
+        Places a list of stringed objects into the list_widget
+        i.e list[Appointments], appointments_list_widget
     
-    def __init__(self):
-        self.name = "Name"
-        self.description = "Description"
+        :param objects: put the list of Object (Models) you want to enter in here
+        :param list_widget: put the QListWidget you want to modify here
 
-    def __str__(self):
-        return f"My name is {self.name} and I am {self.description}"
-
-for i in range(0,10):
-    example_objects.append(
-            test_object()
-        )
-
-
-def place_objects_into_list(objects: list[object], list_widget: QListWidget) -> list[str]:
-    
+        We use the private class QListWidgetObject based off QlistWidgetItem to
+        handle the relationship between data and string
+    """
     list_widget.clear()
-    object_strings = []
+    for obj in objects:
+        list_widget.addItem(__QlistWidgetObject(obj))
+
+def set_objects_to_combo_box(objects: list[object], combo_box: QComboBox):
+    """
+        Places a list of stringed objects into a combo_box
+        i.e list[Physisicans], physiscans_comobo_box
+
+        :param objects: put the list of Object (Models) you want to enter in here
+        :param combo_box: put the QComboBox you want to modify here
+
+        combo_boxes have a special feature that allows us to add data corresponding
+        to the object_str
+    """
+    combo_box.clear()
 
     for obj in objects:
-        object_strings.append(str(obj))
+        combo_box.addItem(str(obj), obj)
 
-    for object_str in object_strings:
-        list_widget.addItem(QListWidgetItem(object_str))
+def get_selected_list_object(list_widget: QListWidget) -> object:
+    """
+        Returns the object that corresponds to whatever is selected in
+        list_widget
 
+        :param list_widget: The QListWidget you wish to read from
 
-    return object_strings
+        :returns: The relevant object placed using our object list setters
+    """
+    items = list_widget.selectedItems()
+    if items:
+        return items[0].obj
 
+def get_selected_combo_box_object(combo_box: QComboBox) -> object:
+    """
+        Returns the object that corresponds to whatever is selected in
+        combo_box
 
-def place_objects_into_combo_box(objects, combo_widget):
-    pass
+        :param combo_box: The QComboBox you wish to read from
+
+        :returns: The relevant object placed using our object box setters
+    """
+    return combo_box.currentData()

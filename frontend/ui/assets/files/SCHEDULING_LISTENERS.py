de0c0bea6ae5400da6d15407fa7b587f4e46e283
weeksjessica <114619757+weeksjessica@users.noreply.github.com>
2023-04-09 12:10:41 -0400

    moved code to class structure

--- frontend/ui/assets/files/SCHEDULING_LISTENERS.py
+++ frontend/ui/assets/files/SCHEDULING_LISTENERS.py
@@ -130,68 +130,8 @@ def clearInputs_SA(self):
                     }
 
         """)
-def getPatientFN_SA(self):
-    return self.SA_PatientFNLineEdit.text()
-def getPatientLN_SA(self):
-    return self.SA_PatientLNLineEdit.text()
-def getPatientDOB_SA(self):
-    return self.SA_PatientDOBDateEdit.date()
-def getOfficeLocation_SA(self):
-    return self.SA_OfficeLocationsComboBox.currentIndex()
-def getAppointmentReason_SA(self):
-    return self.SA_AppointmentReasonLineEdit.text()
-def getAppointmentType_SA(self):
-    return self.SA_AppointmentTypesComboBox.currentIndex()
-def getPhysicianName_SA(self):
-    return self.SA_PhysicianNamesComboBox.currentIndex()
-def getAppointmentDate_SA(self):
-    return self.SA_AppointmentDateDateEdit.date()
-def getAvailableTime_SA(self):
-    return self.SA_CurrentAvailableTimesListWidget.currentRow()
-def getCustomTime_SA(self):
-    if customTime[0] == True:
-        return self.SA_CustomTimeTimeEdit.time()
-def enableCustomTime(self):
 
-        # Appending true to customTime
-        customTime.clear()
-        customTime.append(True)
 
-        self.SA_YesCustomTimePushButton.setVisible(False)
-        self.SA_NoCustomTimePushButton.setVisible(True)
-        self.SA_CurrentAvailableTimesListWidget.clearSelection()
-        self.SA_CustomTimeTimeEdit.setEnabled(True)
-        self.SA_CustomTimeTimeEdit.setStyleSheet("QTimeEdit {\n"
-"    border-image: none;\n"
-"    background-color: rgb(243, 236, 176);\n"
-"    font-family: 'MS Shell Dlg 2';\n"
-"    color: #344D67;;\n"
-"    border: none;\n"
-"    font-size: 15;\n"
-"    padding-left: 10px;\n"
-"    padding-right: 10px;\n"
-"}\n")
-        print("Enabled Custom Time")
-def disableCustomTime(self):
-
-        # Appending false to customTime
-        customTime.clear()
-        customTime.append(False)
-
-        self.SA_YesCustomTimePushButton.setVisible(True)
-        self.SA_NoCustomTimePushButton.setVisible(False)
-        self.SA_CustomTimeTimeEdit.setEnabled(False)
-        self.SA_CustomTimeTimeEdit.setStyleSheet("QTimeEdit {\n"
-"    border-image: none;\n"
-"    background-color: rgba(243, 236, 176, 133);\n"
-"    font-family: 'MS Shell Dlg 2';\n"
-"    color: #344D67;;\n"
-"    border: none;\n"
-"    font-size: 15;\n"
-"    padding-left: 10px;\n"
-"    padding-right: 10px;\n"
-"}\n")
-        print("Disabled Custom Time")
 def checkValidValues_SA(self):
     patientFN = getPatientFN_SA(self)
     patientLN = getPatientLN_SA(self)

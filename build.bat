6169103151953d0c6d94401b1fb598ba3c14e3b5
weeksjessica <114619757+weeksjessica@users.noreply.github.com>
2023-04-11 00:28:26 -0400

    Added proper name stuff

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+:: Creates dist
 python -m PyInstaller --noconfirm --onedir --windowed --debug "all" --paths "%cd%" --add-data "%cd%/backend;backend/" --add-data "%cd%/frontend;frontend/" --hidden-import "greenlet" --hidden-import "pyodbc" --hidden-import "PyQt5" --hidden-import "PyQt5-Qt5" --hidden-import "PyQt5-sip" --hidden-import "SQLAlchemy" --hidden-import "typing_extensions"  "%cd%/main.py"
-
+:: Creates Link to the .exe
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%cd%\main.lnk');$s.TargetPath='%cd%\dist\main\main.exe';$s.Save()"

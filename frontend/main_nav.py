a275f3c01ad02aeb4293ac27d56e576470d8c96e
weeksjessica <114619757+weeksjessica@users.noreply.github.com>
2023-04-12 19:57:15 -0400

    load_error added

--- frontend/main_nav.py
+++ frontend/main_nav.py
@@ -1,5 +1,6 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, QLineEdit, QStackedWidget, QFrame
+from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, QLineEdit, QStackedWidget, QFrame, QDialog, QVBoxLayout, QLabel
+from PyQt5 import QtCore, QtGui
 import sys
 from frontend.ui.assets.files.STYLING import *
 from frontend.ui.assets.qrc import app_bg, doctor, show, hide, logo
@@ -216,6 +217,46 @@ class Nav(QMainWindow):
                 delattr(self, var_name)
 
         sys.exit()
+    
+    def load_error(self, error_text: str):
+        """
+            Loads simple error text box popup with the error_text and a
+            close button
+
+            This can be used for db calls that may return error:
+            try: 
+                add_appointment(appointment)
+            except AssertionError as e: 
+                load_error(error_text=e)
+                return
+            
+            :param error_text: str to be displayed as errors 
+        """
+        infoDialog = QDialog()
+        infoDialog.setStyleSheet(infoDialog_Style)
+
+        # Dialog settings
+        infoDialog.setWindowFlags(QtCore.Qt.FramelessWindowHint) # Hides the title bar
+        infoDialog.setFixedSize(400, 400)
+
+        infoDialogLayout = QVBoxLayout()
+        infoDialogCloseBtn = QPushButton("CLOSE", infoDialog)
+        infoDialogCloseBtn.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+
+        infoDialogCloseBtn.setStyleSheet(infoDialogCloseBtn_Style)
+        infoDialogCloseBtn.setFont(QtGui.QFont("Lato", 12))
+        infoDialogCloseBtn.clicked.connect(infoDialog.close) # Closes the dialog box
+
+        infoDialogName = QLabel(error_text)
+        infoDialogName.setStyleSheet(infoDialogName_Style)
+        infoDialogName.setFont(QtGui.QFont("Lato", 13))
+        infoDialogLayout.addWidget(infoDialogName, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+        infoDialogLayout.addWidget(infoDialogCloseBtn, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+
+        infoDialog.setLayout(infoDialogLayout)
+
+        # Displaying the dialog
+        infoDialog.exec_()
 
 #initializing app
 app = QApplication(sys.argv)

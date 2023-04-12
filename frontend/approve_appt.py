a8415b6915ec647f6e63572aa1c46367e88545b5
weeksjessica <114619757+weeksjessica@users.noreply.github.com>
2023-04-12 19:35:35 -0400

    Added listeners for approve_appt

--- frontend/approve_appt.py
+++ frontend/approve_appt.py
@@ -3,10 +3,30 @@ ApptRequest.py - Window to handle appointment requests from the web portal.
 UI Designed by: Christian Fortin
 Authors: 
 """
+from PyQt5.QtWidgets import QListWidget, QPushButton, QComboBox
 from frontend.lab import Lab
 
 class Approve(Lab):
     def __init__(self):
         super(Approve, self).__init__()
 
-        # TODO: Add buttons
+        self.approve_list = self.findChild(QListWidget, "Approve_List")
+        self.a_btn_approve = self.findChild(QPushButton, "Approve_Btn_Approve")
+        self.a_btn_deny = self.findChild(QPushButton, "Approve_Btn_Deny")
+        self.a_btn_refresh = self.findChild(QPushButton, "Approve_Btn_Refresh")
+        self.a_location = self.findChild(QComboBox, "Approve_Combo_Location")
+        self.a_provider = self.findChild(QComboBox, "Approve_Combo_Provider")
+
+        self.a_btn_approve.mousePressEvent = self.approve
+        self.a_btn_deny.mousePressEvent = self.deny
+        self.a_btn_refresh.mousePressEvent = self.a_refresh
+
+    
+    def approve(self):
+        pass
+
+    def deny(self):
+        pass
+
+    def a_refresh(self):
+        pass

9f2937cd43b6a2e9e0963a5152c6a981b9d33ca7
Jessica Weeks <114619757+weeksjessica@users.noreply.github.com>
2023-04-13 15:29:39 -0400

    Sample Demonstration

--- frontend/lab.py
+++ frontend/lab.py
@@ -5,6 +5,8 @@ Authors:
 """
 from PyQt5.QtWidgets import QPushButton, QLineEdit, QDateEdit, QComboBox
 from frontend.main_nav import Nav
+from backend.sample_data import *
+from backend.data_handler import set_objects_to_combo_box, get_selected_combo_box_object
 class Lab(Nav):
     """
     Creates a lab using Lab object
@@ -19,8 +21,8 @@ class Lab(Nav):
         self.lab_fname = self.findChild(QLineEdit, "LineEdit_PatientFirstName")
         self.lab_lname = self.findChild(QLineEdit, "LineEdit_PatientLastName")
         self.lab_dob = self.findChild(QDateEdit, "dateEdit_DOB")
-        self.lab_practitioner_combo = self.findChild(QComboBox, "comboBox_Practitioner")
-        self.lab_locations_combo = self.findChild(QComboBox, "comboBox_LocationID")
+        self.lab_practitioner_combo = self.findChild(QComboBox, "Lab_Combo_Practitioner")
+        self.lab_locations_combo = self.findChild(QComboBox, "Lab_Combo_Location")
         self.lab_lab_date_edit = self.findChild(QDateEdit, "dateEdit_LabDate")
         self.lab_lab_combo = self.findChild(QComboBox, "comboBox_PossibleLabs")
         self.lab_order_name = self.findChild(QLineEdit, "LineEdit_LabOrderName")
@@ -28,6 +30,15 @@ class Lab(Nav):
         # Do something
         self.lab_submit_btn.clicked.connect(self.submit_information)
         self.lab_clear_btn.clicked.connect(self.clearInputs)
+
+        # SAMPLE DATA STUFF
+        set_objects_to_combo_box(
+            locations, self.lab_locations_combo
+        )
+
+        set_objects_to_combo_box(
+            employees, self.lab_practitioner_combo
+        )
 
 
     def submit_information(self):
@@ -42,4 +53,12 @@ class Lab(Nav):
             
             Then run self.clear_inputs()
         """
-        print(self.submit_information.__doc__)
+        #print(self.submit_information.__doc__)
+
+        selected_emp = get_selected_combo_box_object(
+            self.lab_practitioner_combo
+        )
+
+        print(" Selected Employee Is:", selected_emp)
+        print(" We have the access to the whole object: ")
+        print(" Position:", selected_emp.Position)

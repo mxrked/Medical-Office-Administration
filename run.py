d37bb29d982d8947f692d877d073daf904f11273
weeksjessica <114619757+weeksjessica@users.noreply.github.com>
2023-04-15 09:37:10 -0400

    Unnessary Code

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from PyQt5.QtWidgets import *
-from PyQt5 import uic, QtCore, QtGui
-import sys
-from frontend.ui.assets.qrc import logo
+from datetime import datetime, date
 from frontend.StartWindow import main
 
 if __name__ == "__main__":
-    main(debug=True)
+    main(
+        debug=True,
+        today=date(2023,5,2)
+    )

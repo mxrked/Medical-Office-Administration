0020c039a3bb99f6e5b0f342015adf8ecbd17c09
Parker <pp101socials@gmail.com>
2023-04-15 09:30:40 -0400

    Added data to comboboxes

--- frontend/referral.py
+++ frontend/referral.py
@@ -6,6 +6,9 @@ Authors:
 
 from PyQt5.QtWidgets import QLineEdit, QDateEdit, QComboBox, QPushButton
 from frontend.main_nav import Nav
+from backend.data_handler import set_objects_to_combo_box
+from backend.user_dm import UserDM
+
 class Referral(Nav):
     """
         Creates a referral using the Referral object
@@ -19,11 +22,13 @@ class Referral(Nav):
     def __init__(self):
         super(Referral, self).__init__()
 
+        Referral_Physicians = UserDM().get_physicians()
+
         # Initalized Input Widgets
         self.ref_fname = self.findChild(QLineEdit, "LineEdit_PatientFirstName")
         self.ref_lname = self.findChild(QLineEdit, "LineEdit_PatientLastName")
         self.dob = self.findChild(QDateEdit, "DateEdit_DOB")
-        self.ref_practitioners = self.findChild(QComboBox, "ComboBox_DoctorPractitioner")
+        self.ref_practitioners = self.findChild(QComboBox, "Referral_Practitoner")
         self.ref_creation_date = self.findChild(QDateEdit, "DateEdit_CreationDate")
         self.ref_reason = self.findChild(QLineEdit, "LineEdit_Reason")
 
@@ -32,6 +37,8 @@ class Referral(Nav):
 
         # Button Listeners
         self.create_referral_btn.clicked.connect(self.create_referral)
+
+        set_objects_to_combo_box(Referral_Physicians, self.ref_practitioners)
 
 
     def create_referral(self):

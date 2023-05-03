3159814d984b7f944288e77d5f3aabe03cf2f6bd
cfortin99 <cfortin99@gmail.com>
2023-05-03 08:48:47 -0400

    Update utility.py

--- frontend/private/utility.py
+++ frontend/private/utility.py
@@ -1,10 +1,10 @@
 """
-utility.py - A bunch of utlity functions that are used by every screen.
+utility.py - A bunch of utility functions that are used by every screen.
 Author: Jessica Weeks
 """
 import sys 
 from datetime import date
-from PyQt5.QtWidgets import QWidget, QLineEdit, QDateEdit, QTimeEdit, QDialog, \
+from PyQt5.QtWidgets import QWidget, QLineEdit, QDialog, \
     QVBoxLayout, QLabel, QDateEdit, QTimeEdit, QComboBox, QPushButton, QTextEdit, QRadioButton, QListWidget
 from PyQt5.QtCore import QDate, Qt
 from PyQt5 import QtGui
@@ -22,7 +22,7 @@ class Utility(Nav):
     """
         Handles general utility that all screens use
 
-        • Initalizing data handlers
+        • Initializing data handlers
         • clearInputs (Clears ALL inputs)
         • closeEvent (What happens when program closes)
         • 
@@ -36,7 +36,6 @@ class Utility(Nav):
         self.misc_dm = MiscDM()
         self.appointment_dm = AppointmentDM()
 
-    
     def clear_inputs(self):
         """
             Resets all widgets everywhere!
@@ -44,26 +43,31 @@ class Utility(Nav):
         """
         for widget in self.findChildren(QWidget):
             # Clears Line Edits
-            if isinstance(widget, QLineEdit) and not isinstance(widget, (QTimeEdit, QDateEdit, QRadioButton, QTextEdit, QListWidget)):
+            if isinstance(widget, QLineEdit) and not isinstance(widget, (QTimeEdit, QDateEdit, QRadioButton, QTextEdit,
+                                                                         QListWidget)):
                 widget.clear()
                 # Otherwise Certain widgets are selected, for some reason
                 widget.selectAll()
                 widget.deselect()
 
             # Clears DateEdits
-            if isinstance(widget, QDateEdit) and not isinstance(widget, (QTimeEdit, QTextEdit, QLineEdit, QListWidget, QRadioButton)):
+            if isinstance(widget, QDateEdit) and not isinstance(widget, (QTimeEdit, QTextEdit, QLineEdit, QListWidget,
+                                                                         QRadioButton)):
                 widget.setDate(QDate.currentDate())
 
             # Clear Text Edit
-            if isinstance(widget, QTextEdit) and not isinstance(widget, (QTimeEdit, QDateEdit, QLineEdit, QListWidget, QRadioButton)):
+            if isinstance(widget, QTextEdit) and not isinstance(widget, (QTimeEdit, QDateEdit, QLineEdit, QListWidget,
+                                                                         QRadioButton)):
                 widget.clear()
 
             # Clear Radio Btn
-            if isinstance(widget, QRadioButton) and not isinstance(widget, (QTimeEdit, QDateEdit, QLineEdit, QTextEdit, QListWidget)):
+            if isinstance(widget, QRadioButton) and not isinstance(widget, (QTimeEdit, QDateEdit, QLineEdit, QTextEdit,
+                                                                            QListWidget)):
                 widget.setChecked(False)
 
             # Clear List Widgets
-            if isinstance(widget, QListWidget) and not isinstance(widget, (QTimeEdit, QDateEdit, QLineEdit, QTextEdit, QRadioButton)):
+            if isinstance(widget, QListWidget) and not isinstance(widget, (QTimeEdit, QDateEdit, QLineEdit, QTextEdit,
+                                                                           QRadioButton)):
                 widget.clear()
     
     def closeEvent(self, event): # Do not change name, needs to be the same as QMainWindow's
@@ -77,7 +81,6 @@ class Utility(Nav):
             dm.close()
 
         sys.exit()
-    
 
     def load_error(self, error_text: str):
         """
@@ -97,7 +100,7 @@ class Utility(Nav):
         info_dialog.setStyleSheet(infoDialog_Style)
 
         # Dialog settings
-        info_dialog.setWindowFlags(Qt.FramelessWindowHint) # Hides the title bar
+        info_dialog.setWindowFlags(Qt.FramelessWindowHint)  # Hides the title bar
         info_dialog.setFixedSize(400, 400)
 
         info_layout = QVBoxLayout()
@@ -106,7 +109,7 @@ class Utility(Nav):
 
         info_close_btn.setStyleSheet(infoDialogCloseBtn_Style)
         info_close_btn.setFont(QtGui.QFont("Lato", 12))
-        info_close_btn.clicked.connect(info_dialog.close) # Closes the dialog box
+        info_close_btn.clicked.connect(info_dialog.close)  # Closes the dialog box
 
         info_label = QLabel(error_text)
         info_label.setStyleSheet(infoDialogName_Style)
@@ -119,14 +122,13 @@ class Utility(Nav):
         # Displaying the dialog
         info_dialog.exec_()
 
-
     def load_locations(self, combo_box: QComboBox):
         """
             Loads all locations into the combo_box,
-                just provide the elevant data_manager
+                just provide the relevant data_manager
 
             :param misc_dm: use self.misc_dm
-            :param combo_box: The combo box you wanna add stuff too
+            :param combo_box: The combo box you want to add stuff to
             
             We do this several times in the frontend. 
                 This prevents duplicate code
@@ -138,15 +140,14 @@ class Utility(Nav):
         load_objects_to_combo_box(locations, combo_box)
         combo_box.setCurrentIndex(int(default_location))
 
-
     def load_physicians(self,
                         combo_box: QComboBox,
-                        location: Location=None):
+                        location: Location = None):
         """
             Loads all physicians into the combo_box,
                 just provide the relevant data_manager
 
-            :param combo_box: The combo box you wanna add stuff too
+            :param combo_box: The combo box you want to add stuff to
             :param location: The location object or a string will be "All"
 
             We do this several times in the frontend. 
@@ -164,7 +165,7 @@ class Utility(Nav):
 
     def get_verified_patient(self, f_name: str, l_name: str, dob: date) -> Patient:
         """
-            In multiple sections we need to get a patient and verify (with a assert)
+            In multiple sections we need to get a patient and verify (with an assert)
                 that one was found. This handles that
 
             This also handles if more than 2 patients are found
@@ -175,10 +176,10 @@ class Utility(Nav):
         """
         
         patients = self.user_dm.get_patients(first_name=f_name,
-                                                    last_name=l_name,
-                                                    dob=dob)
+                                             last_name=l_name,
+                                             dob=dob)
 
-        assert len(patients) != 0 , "No Patients Found"
+        assert len(patients) != 0, "No Patients Found"
         if len(patients) > 1:
             # Removes all but the selected item from the dialog
             patients = [self.display_settings_dialog(patients)]
@@ -187,10 +188,9 @@ class Utility(Nav):
 
         return patients[0]
 
-
     def display_settings_dialog(self, patients: list[Patient]):
         """
-            Pops up a dialog window for the soul purpose of resolving mutltiple patients
+            Pops up a dialog window for the soul purpose of resolving multiple patients
                 User not allowed to leave until a patient is selected
 
             :param patients: list of Patient Objects
